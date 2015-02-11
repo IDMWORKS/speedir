d9b8979a065316160679b970c5da1d6f3bfebb88
Nate Woolls <nathanial.woolls@idmworks.com>
2015-02-10 23:03:44 -0500

    Refactor speedir main - extract methods

--- speedir.go
+++ speedir.go
@@ -6,43 +6,55 @@ import (
 	"github.com/idmworks/speedir/datacontext"
 	"github.com/idmworks/speedir/processor"
 	"github.com/idmworks/speedir/server"
+	"gopkg.in/gorp.v1"
 )
 
 const (
 	listenTCPPort = 3333
 	listenTLSPort = 3334
+	dbname        = "speedir"
+	dbuser        = "speedir"
 )
 
-var verbose = false
+var (
+	verbose = false
+)
 
 func main() {
 	parseFlags()
-
-	//initialize DB schema
-	dbmap := datacontext.InitDb("speedir", "speedir")
-	//close DB when app exits
-	defer dbmap.Db.Close()
-	//seed DB data
-	datacontext.SeedDb(dbmap)
-
-	//give processor access to data
-	processor.DbMap = dbmap
-	//set processor verbosity
-	processor.Verbose = verbose
-
-	//start first TCP server in a goroutine
-	go server.ServeTCP(listenTCPPort, false, processor.HandleRequest)
-	//start second TCP (TLS) server in the main thread
-	server.ServeTCP(listenTLSPort, true, processor.HandleRequest)
+	dbmap := setupDb()
+	defer closeDb(dbmap)
+	setupProcessor(dbmap)
+	startServers()
 }
 
 func parseFlags() {
-	//register flags & pointers where values will be stored
-	value := flag.Bool("verbose", false, "verbose output")
-
-	//parse all flags - values now stored in pointers
+	// register flags & pointers where values will be stored
+	verbosePtr := flag.Bool("verbose", false, "verbose output")
+	// parse all flags - values now stored in pointers
 	flag.Parse()
+	// store flags for use throughout the app
+	verbose = *verbosePtr
+}
 
-	//store flags for use throughout the app
-	verbose = *value
+func setupDb() *gorp.DbMap {
+	dbmap := datacontext.InitDb(dbname, dbuser)
+	datacontext.SeedDb(dbmap)
+	return dbmap
+}
+
+func closeDb(dbmap *gorp.DbMap) {
+	dbmap.Db.Close()
+}
+
+func setupProcessor(dbmap *gorp.DbMap) {
+	processor.DbMap = dbmap
+	processor.Verbose = verbose
+}
+
+func startServers() {
+	// start first TCP server in a goroutine
+	go server.ServeTCP(listenTCPPort, false, processor.HandleRequest)
+	// start second TCP (TLS) server in the main thread
+	server.ServeTCP(listenTLSPort, true, processor.HandleRequest)
 }
