097c9cd1dbf8782ba7938a41ff5833b4262dcb14
Nate Woolls <nathanial.woolls@idmworks.com>
2015-02-10 19:48:25 -0500

    Do not close the TCP connection  - loop & read

--- processor/processor.go
+++ processor/processor.go
@@ -15,22 +15,24 @@ var DbMap *gorp.DbMap
 
 // HandleRequest handles incoming LDAPv3 requests
 func HandleRequest(conn net.Conn) {
-	defer conn.Close()
+	// continuously read from the connection
+	for {
+		packet, err := ber.ReadPacket(bufio.NewReader(conn))
+		if err != nil {
+			defer conn.Close()
+			log.Println("Error reading:", err.Error())
+			return
+		}
 
-	packet, err := ber.ReadPacket(bufio.NewReader(conn))
-	if err != nil {
-		log.Println("Error reading:", err.Error())
-		return
+		// required to catch issues like TLS/TCP port mis-matches
+		if len(packet.Children) == 0 {
+			defer conn.Close()
+			log.Println("Error decoding asn1-ber packet: wrong port?")
+			return
+		}
+
+		parsePacket(conn, packet)
 	}
-
-	//required to catch issues like TLS/TCP port mis-matches
-	if len(packet.Children) == 0 {
-		defer conn.Close()
-		log.Println("Error decoding asn1-ber packet: wrong port?")
-		return
-	}
-
-	parsePacket(conn, packet)
 }
 
 func parsePacket(conn net.Conn, packet *ber.Packet) {
